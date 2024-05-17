0f6f16e23392b38b716c4e794ec6d64bc61b14c3
Aleksandr Petrov <firexel@gmail.com>
2024-05-17 10:16:00 +0100

    shrank the figure

--- simulated_experiment_plot.py
+++ simulated_experiment_plot.py
@@ -7,19 +7,23 @@ data= data[["num_items", "item_code_bytes",	"mRT", "method"]]
 mm_data = data[data.method == "Matrix Multiplication"]
 print(mm_data)
 
-markers = [".", "^", "x", "d", "D"]
+markers = ["o", "^"]
+linesize=2
+markersize=5
 for item_code_bytes in data.item_code_bytes.unique():
     code_data = data[data.item_code_bytes == item_code_bytes]
-    fig, ax = plt.subplots(1, 1, figsize=(4, 2.4))
+    fig, ax = plt.subplots(1, 1, figsize=(2.8, 2.2))
     for i, method in enumerate(["PQTopK", "RecJPQ"]):
         method_code_data = code_data[code_data.method == method]
-        ax.plot(method_code_data["num_items"], method_code_data["mRT"], label=method, marker = markers[i])
+        ax.plot(method_code_data["num_items"], method_code_data["mRT"], label=method, marker = markers[i], linewidth=linesize, markersize=markersize)
         pass
-    ax.plot(mm_data["num_items"], mm_data["mRT"], label="Transformer Default", linestyle="--")
+    ax.plot(mm_data["num_items"], mm_data["mRT"], label="Default", linestyle="solid", linewidth=linesize, markersize=markersize, marker = "x")
     ax.set_xlabel("Number of Items")
     ax.set_ylabel("mRT (ms)")
     ax.set_xscale('log')
     ax.set_yscale('log')
+    ax.set_yticks([1e0, 1e1, 1e2, 1e3, 1e4])
+    ax.set_xticks([1e4, 1e5, 1e6, 1e7, 1e8, 1e9])
     ax.grid()
     ax.legend(prop={'size': 8})
     fig.tight_layout()
